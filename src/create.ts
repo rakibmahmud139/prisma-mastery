bd904142369ba83afc4371e1b836beff1a4732f7
Rakibmahmud <hasansaikat74@gmail.com>
2024-03-10 21:38:19 +0600

    added relation

--- src/create.ts
+++ src/create.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, UserRole } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -14,27 +14,71 @@ const main = async () => {
   //   console.log(result);
 
   //create many
-  const createMany = await prisma.post.createMany({
-    data: [
-      {
-        title: "Title 4",
-        content: "Content 4",
-        authorName: "Author 4",
-      },
-      {
-        title: "Title 5",
-        content: "Content 5",
-        authorName: "Author 5",
-      },
-      {
-        title: "Title 6",
-        content: "Content 6",
-        authorName: "Author 6",
-      },
-    ],
-  });
+  // const createMany = await prisma.post.createMany({
+  //   data: [
+  //     {
+  //       title: "Title 4",
+  //       content: "Content 4",
+  //       authorName: "Author 4",
+  //     },
+  //     {
+  //       title: "Title 5",
+  //       content: "Content 5",
+  //       authorName: "Author 5",
+  //     },
+  //     {
+  //       title: "Title 6",
+  //       content: "Content 6",
+  //       authorName: "Author 6",
+  //     },
+  //   ],
+  // });
 
-  console.log(createMany);
+  // console.log(createMany);
+
+  // const createUser = await prisma.user.create({
+  //   data: {
+  //     username: "rakib2",
+  //     email: "rakib@gmail.com",
+  //     role: UserRole.user,
+  //   },
+  // });
+
+  // const createProfile = await prisma.profile.create({
+  //   data: {
+  //     bio: "This is bio",
+  //     userId: 1,
+  //   },
+  // });
+
+  // const createCategory = await prisma.category.create({
+  //   data: {
+  //     name: "Software Engineering",
+  //   },
+  // });
+
+  const createPost = await prisma.post.create({
+    data: {
+      title: "This is title",
+      content: "This is content of the post",
+      authorId: 1,
+      postCategory: {
+        create: {
+          categoryId: 3,
+
+          // category: {
+          //   connect: {
+          //     id: 1,
+          //   },
+          // },
+        },
+      },
+    },
+    include: {
+      postCategory: true,
+    },
+  });
+  console.log(createPost);
 };
 
 main();
